0c5a25f71c563e6e4b175b38818268be286d330d
Isiaka Abdulahi <isiakaabd@gmail.com>
2021-02-23 03:16:23 +0100

    removing console.log

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const range = document.getElementById("range");
 
 
 range.addEventListener("input", (e)=>{
- const   target = e.target
+const target = e.target
 const value = target.value;
 const label = target.nextElementSibling   // or  const label = target.nextElementSibling
 
@@ -12,7 +12,7 @@ const range_width = getComputedStyle(target).getPropertyValue("width");
 const label_width = getComputedStyle(label).getPropertyValue("width");
 //The range_width comes with px e.g 300px, The px needs to be removed to get the exact value
 const range_value = range_width.substring(0, range_width.length - 2);
-const label_value = +label_width.substring(0, label_width.length - 2);
+const label_value = label_width.substring(0, label_width.length - 2);
 //max and min
 const max= target.max
 const min= target.min
@@ -20,7 +20,7 @@ const min= target.min
 // moving the label inrespect to the value
 const left = value * (range_value / max) - label_value / 2 + scale(value, min, max, 10, -10)
 label.style.left = `${left}px`;
-console.log(left)
+
 
 label.innerHTML = value
 })
